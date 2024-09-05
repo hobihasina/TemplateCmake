9ecceedb3c5e01f62e65e873772800128052031f
Hobihasina Patrick RAKOTOARISOA <hrakotoarisoa@groupeastek.mg>
2024-09-05 16:42:42 +0300

    corretion on format-code

--- src/calculator.h
+++ src/calculator.h
@@ -1,8 +1,8 @@
 class Calculator {
-public:
-    int add (int, int);
-    int sub (int, int);
-    int mul (int, int);
-    int div (int, int);
-    int mod (int, int);
+ public:
+  int add(int, int);
+  int sub(int, int);
+  int mul(int, int);
+  int div(int, int);
+  int mod(int, int);
 };
